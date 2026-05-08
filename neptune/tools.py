cd75c1ad40f18545e32eb9c14c294ad532861852
Victor Mangeleer <vmangeleer@uliege.be>
2026-05-08 11:47:05 +0200

    🧮 Adding New Tool - Introducing new tool to extract all dict combinations from .yml file. This is used to ease the launch of multiple jobs at once

---
+++ neptune/tools.py
@@ -0,0 +1,26 @@
+r"""A collection of tools designed for training module."""
+
+import yaml
+
+from itertools import product
+from pathlib import Path
+from typing import Any
+
+
+def load_configuration(path: Path) -> list[dict[str, Any]]:
+    r"""Load all combinations of parameters from a YAML configuration file."""
+
+    def _generate_combinations(d: dict[str, Any]) -> list[dict[str, Any]]:
+        r"""Recursively generate parameter combinations."""
+        if isinstance(d, dict):
+            combinations = {k: _generate_combinations(v) for k, v in d.items()}
+            keys, values = zip(*combinations.items(), strict=False)
+            return [dict(zip(keys, combo, strict=False)) for combo in product(*values)]
+        return d if isinstance(d, list) else [d]
+
+    # Open and read the YAML configuration file
+    with open(path) as file:
+        config = yaml.safe_load(file)
+
+    # Generate combinations
+    return _generate_combinations(config)
