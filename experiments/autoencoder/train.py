3a062f511bcc663b0022f140a800eb34c904dbd8
Victor Mangeleer <vmangeleer@uliege.be>
2026-05-12 11:29:44 +0200

    🪒 Second Draft of Training Script

--- experiments/autoencoder/train.py
+++ experiments/autoencoder/train.py
@@ -6,11 +6,13 @@ import dawgz
 import torch
 import torch.distributed as dist
 import wandb
-import yaml
 
+from omegaconf import OmegaConf
 from shaggy.loss import AELoss
 from shaggy.models.cae import create_ConvAE
 from shaggy.optimizer import SOAP, safe_gradient_step
+from shaggy.tools import load as s_load
+from shaggy.tools import save as s_save
 from torch.amp.grad_scaler import GradScaler
 from torch.nn.parallel import DistributedDataParallel as DDP
 
@@ -18,7 +20,7 @@ from neptune.config import PATH_MODELS
 from neptune.data import DATASET_REGION, DATASET_VARIABLES_OCEAN, DATASET_VARIABLES_SURFACE
 from neptune.data.dataloader import get_dataloaders
 from neptune.data.weights import get_weights_loss, get_weights_mask
-from neptune.distributed import setup_distributed
+from neptune.distributed import reduce_mean, setup_distributed
 from neptune.tools import load_configuration
 
 
@@ -57,6 +59,7 @@ def training(
         steps_update,
         steps_logging,
         steps_validation,
+        steps_saving,
         samples_validation,
         batch_size_per_step,
         batch_size_per_gpu,
@@ -67,6 +70,7 @@ def training(
         config_training["steps_update"],
         config_training["steps_logging"],
         config_training["steps_validation"],
+        config_training["steps_saving"],
         config_training["samples_validation"],
         config_training["batch_size_per_step"],
         config_training["batch_size_per_gpu"],
@@ -75,19 +79,26 @@ def training(
         config_training["prefetch_factor"],
     )
 
-    # Number of batches to yield per dataloader
-    batches = [
-        max(1, (steps_update * batch_size_per_step + batch_size_per_gpu - 1) // batch_size_per_gpu),
-        max(1, (samples_validation + batch_size_per_gpu - 1) // batch_size_per_gpu),
-        None,
-     ]
+    # Number of steps to accumulate gradients before updating model parameters
+    batch_size_per_process = batch_size_per_gpu * world_size
+    steps_gradient_accumulation = max(
+        1, (batch_size_per_step + batch_size_per_process - 1) // batch_size_per_process
+    )
 
-    dataloader_training, _, _ = get_dataloaders(
+    # Number of batches to yield per dataloader
+    n_batches_validation = max(1, (samples_validation + batch_size_per_gpu - 1) // batch_size_per_gpu)
+    batches = [
+        steps_update * steps_gradient_accumulation,
+        steps_update * n_batches_validation,
+        None,
+    ]
+
+    dataloader_training, dataloader_validation, _ = get_dataloaders(
         batch_size      = batch_size_per_gpu,
         num_workers     = num_workers,
         prefetch_factor = prefetch_factor,
         batches         = batches,
-        shuffle         = [True, False, False],
+        shuffle         = [True,  True, False],
         infinite        = [True, True, False],
         rank            = rank,
         world_size      = world_size,
@@ -99,26 +110,21 @@ def training(
         dist.barrier()
 
     # Constants
-    Z     = DATASET_REGION["deptht"].stop - DATASET_REGION["deptht"].start      # Depth levels
-    C     = len(DATASET_VARIABLES_SURFACE) + len(DATASET_VARIABLES_OCEAN) * Z   # Aggregated levels
-    IN_C  = C + Z                                                               # State = Variables + Mask
-    OUT_C = C                                                                   # State = Variables
+    Z     = DATASET_REGION["deptht"].stop - DATASET_REGION["deptht"].start     # Depth levels
+    C     = len(DATASET_VARIABLES_SURFACE) + len(DATASET_VARIABLES_OCEAN) * Z  # Aggregated levels
+    IN_C  = C + Z                                                              # State = Variables + Mask
+    OUT_C = C                                                                  # State = Variables
 
     # Initializing weighting tensors
     w_mask, w_loss = (
-        get_weights_mask(dim=2,             device=device), # (1,     Z, Y, X)
-        get_weights_loss(dim=2, scale=10.0, device=device), # (1, OUT_C, 1, 1)
+        get_weights_mask(dim=2,              device=device), # (1,     Z, Y, X)
+        get_weights_loss(dim=2, scale=100.0, device=device), # (1, OUT_C, 1, 1)
     )
 
     # Model | 1 | Loading checkpoint
     if config_state["checkpoint_name"] is not None:
         ckpt_path = PATH_MODELS / config_state["checkpoint_name"]
-        with open(ckpt_path / "config.yml") as f:
-            ckpt_config = yaml.safe_load(f)
-        state_dict = torch.load(ckpt_path / "model.pth", map_location=device, weights_only=True)
-        model = create_ConvAE(**ckpt_config).to(device)
-        model.load_state_dict(state_dict)
-        model.train()
+        model     = s_load(ckpt_path, device=str(device)).train()
 
     # Model | 2 | New
     else:
@@ -130,11 +136,12 @@ def training(
 
     # Model | 3 | Defining if DDP or DataParallel
     if is_distributed:
-        model = DDP(
-            model,
-            device_ids=[local_rank],
-            output_device=local_rank,
-        )
+        ddp_kwargs = {
+            "device_ids": [local_rank],
+            "output_device": local_rank
+        } if device.type == "cuda" else {}
+
+        model = DDP(model, **ddp_kwargs)
 
     elif torch.cuda.device_count() > 1:
         model = torch.nn.DataParallel(
@@ -145,23 +152,19 @@ def training(
     # Logging number of trainable parameters
     if rank == 0:
         wandb.log({
-            "Neural Network/Trainable Parameters": sum(
+            "Informations/Trainable Parameters [M]": sum(
                 p.numel() for p in model.parameters() if p.requires_grad
-            ),
+            ) / 1e6,
         })
 
     # Setting up training tools
-    optimizer                   = SOAP(model.parameters(), lr=learning_rate)
-    scheduler                   = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda _: 1.0)
-    scaler                      = GradScaler(enabled=True)
-    loss_function               = AELoss(weights=w_loss)
-    loss_accumulator            = 0.0
-    optimizer_step              = 0
-    steps_gradient_accumulation = max(
-        1,
-        batch_size_per_step // (batch_size_per_gpu * world_size) if is_distributed \
-        else batch_size_per_step // batch_size_per_gpu,
-    )
+    optimizer                = SOAP(model.parameters(), lr=learning_rate)
+    scheduler                = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda _: 1.0)
+    scaler                   = GradScaler(enabled=True)
+    loss_function            = AELoss(weights=w_loss)
+    loss_accumulator         = 0.0
+    loss_logging_accumulator = 0.0
+    optimizer_step           = 0
 
     # Waiting for processes to be ready (2)
     if is_distributed:
@@ -199,30 +202,91 @@ def training(
 
         # Optimization step
         if is_last_accumulation_step:
-            gradient_norm    = safe_gradient_step(optimizer=optimizer, scaler=scaler, grad_clip=1.0)
-            loss_to_log      = loss_accumulator
-            loss_accumulator = 0.0
-            optimizer_step  += 1
+            gradient_norm             = safe_gradient_step(optimizer=optimizer, scaler=scaler, grad_clip=1.0)
+            loss_to_log               = loss_accumulator
+            loss_logging_accumulator += loss_to_log
+            loss_accumulator          = 0.0
+            optimizer_step           += 1
             scheduler.step()
             del loss
 
         # Logging results
-        if optimizer_step % steps_logging == 0 and rank == 0 and is_last_accumulation_step:
+        if optimizer_step % steps_logging == 0 and is_last_accumulation_step:
 
-            # Computing the mean loss over logging steps
-            loss_mean = (loss_to_log / steps_gradient_accumulation) / steps_logging
+            # Average loss over logging window
+            loss_mean                = loss_logging_accumulator / steps_logging
+            loss_logging_accumulator = 0.0
+
+            # Average across distributed processes
+            if is_distributed:
+                loss_mean = reduce_mean(loss_mean, device)
 
             # Logging
-            wandb.log({
-                "Train/Loss": loss_mean,
-                "Train/Steps Update": optimizer_step,
-                "Train/Samples Seen": (step + 1) * batch_size_per_gpu * world_size,
-                "Train/Gradient Norm": gradient_norm,
-            })
+            if rank == 0:
+                wandb.log({
+                    "Training & Validation/Loss (Training)" : loss_mean,
+                    "Informations/Steps Update [-]"         : optimizer_step,
+                    "Informations/Samples Seen [-]"         : (step + 1) * batch_size_per_gpu * world_size,
+                    "Informations/Gradient Norm [-]"        : gradient_norm,
+                })
 
         # Computing validation
-        if optimizer_step % steps_validation == 0:
-            pass
+        if optimizer_step % steps_validation == 0 and is_last_accumulation_step and optimizer_step > 0:
+
+            # Switching to evaluation mode (dropout)
+            model.eval()
+            val_loss, n_val_batches = 0.0, 0
+
+            with torch.no_grad():
+                for x_val, _ in dataloader_validation:
+
+                    # Pushing to device and concatenating mask
+                    x_val = x_val.to(device)
+                    x_val = torch.cat([x_val, w_mask.expand(x_val.shape[0], -1, -1, -1)], dim=1)
+
+                    with torch.amp.autocast(device_type=device.type, dtype=torch.bfloat16):
+
+                        # Forward pass
+                        _, x_hat_val = model(x_val)
+
+                        # Computing loss
+                        val_loss += loss_function(x_hat_val, x_val[:, :C]).item()
+
+                    # Cleaning
+                    del x_val, x_hat_val
+
+                    # Dataloader is infinite, we need to manually break the loop after enough batches seen
+                    n_val_batches += 1
+                    if n_val_batches >= n_batches_validation:
+                        break
+
+            # Computing mean validation loss accross batches and processes
+            val_loss_mean = val_loss / max(1, n_val_batches)
+            if is_distributed:
+                val_loss_mean = reduce_mean(val_loss_mean, device)
+
+            # Logging results
+            if rank == 0:
+                wandb.log({"Training & Validation/Loss (Validation)": val_loss_mean})
+
+            # Switching back to training mode
+            model.train()
+
+        # Saving checkpoint
+        if optimizer_step % steps_saving == 0 and is_last_accumulation_step and optimizer_step > 0 and rank == 0:
+
+            # Extracting raw model from DDP/DataParallel wrapper if needed
+            raw_model = model.module if hasattr(model, "module") else model
+
+            # Creating checkpoint configuration
+            ckpt_config = OmegaConf.create({
+                "in_channels": IN_C,
+                "out_channels": OUT_C,
+                **config_arch
+            })
+
+            # Saving model (overwrites previous checkpoint for this run)
+            s_save(raw_model, ckpt_config, PATH_MODELS / wandb.run.name)
 
     # Closing run
     wandb.finish()
@@ -283,6 +347,7 @@ if __name__ == "__main__":
             interpreter = f"torchrun --nnodes 1 --nproc-per-node {gpus_per_node} --standalone"
 
         @dawgz.job(
+            array=len(configs),
             nodes=nodes,
             gpus=gpus_per_node,
             cpus=cpus_per_node,
@@ -290,8 +355,6 @@ if __name__ == "__main__":
             time=config_cluster.get("time", "00:10:00"),
             account=config_cluster.get("account"),
             partition=config_cluster.get("partition"),
-            interpreter=interpreter,
-            export="ALL",
         )
         def train(i: int) -> None:
             ae = configs[i]["Autoencoder"]
@@ -303,5 +366,10 @@ if __name__ == "__main__":
                 config_cluster=config_cluster,
             )
 
-        jobs = [train(i) for i in range(len(configs))]
-        dawgz.schedule(*jobs, name="AE-TRAIN", backend="slurm")
+        dawgz.schedule(
+            train,
+            name="AE-TRAIN",
+            backend="slurm",
+            interpreter=interpreter,
+            export="ALL"
+        )
